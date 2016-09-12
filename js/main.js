995e7defa2ab288b0f73eaab3f47d0208f189e87
Gleb Starkov <glebstarkov@gmail.com>
2016-09-12 11:18:11 +0800

    Добавил вариант карты с линиями маршрутов и остановками

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
  */
 
 var max_points = 300;
+var currrent_user = 0;
+var current_map = 'vector';
 
 $(document.ready = function(){
     $('.j-max-points input').val(max_points);
@@ -39,6 +41,7 @@ $(document.ready = function(){
             selfId = data.user.id;
             $('#head-user-name').html(data.user.name);
             $('.j-my-trac-btn').removeClass('hidden');
+            currrent_user = selfId;
             setMyTrac();
         });
         
@@ -62,6 +65,13 @@ $(document.ready = function(){
         max_points = parseInt($('.j-max-points input').val());
         setMyTrac();
     });
+
+    $('.j-change-map').on('click', function(){
+        $('.j-change-map').removeClass('active');
+        $(this).addClass('active');
+        current_map = $(this).attr('data-view');
+        showMap(currrent_user);
+    });
 });
 
 function setFriend(obj) {
@@ -69,7 +79,9 @@ function setFriend(obj) {
     $('.j-friend-item').removeClass('active');
     $(obj).addClass('active');
 
-    showMap($(obj).attr('data-id'));
+    currrent_user = $(obj).attr('data-id');
+
+    showMap(currrent_user);
 
     return false;
 }
@@ -78,6 +90,7 @@ function setMyTrac()
 {
     $('.j-my-trac-btn').addClass('active');
     $('.j-friend-item').removeClass('active');
+    currrent_user = selfId;
     showMap(selfId);
 
     return false;
@@ -86,6 +99,10 @@ function setMyTrac()
 function showMap(userId)
 {
     $('#first_map').html('');
+
+    if ('vector' == current_map) {
+        return showMapVectors(userId);
+    }
 
     data = {
         url: '/geo/points?token=' + _token,
@@ -150,30 +167,132 @@ function showMap(userId)
                 cnt++;
             }
         });
-
-        /*
-         var route = [];
-
-         for(key in points) {
-         route.push({
-         type: 'wayPoint',
-         point: [
-         points[key].lat,
-         points[key].lan
-         ]
-         });
-         }
-
-         ymaps.route(route, {
-         mapStateAutoApply: true
-         }).then(
-         function (route) {
-         friend_map.geoObjects.add(route);
-         },
-         function (error) {
-         alert("Возникла ошибка: " + error.message);
-         }
-         );
-         */
     });
+}
+
+function showMapVectors(userId)
+{
+    data = {
+        url: '/geo/vectors?token=' + _token,
+        id: userId
+    };
+
+    $.post('/curl.php', data, function (data) {
+        if (undefined == data.points) {
+            showLogin();
+            return false;
+        }
+
+        var points = [];
+
+        var max = max_points - 1;
+
+        if ((data.points.length - 1) < max) {
+            max = data.points.length - 1;
+        }
+
+        for(var i=0; i<=max; i++) {
+            points.push({
+                geo: data.points[i].geo,
+                lat: data.points[i].geo.replace(/^(.+),.+$/, '$1'),
+                lon: data.points[i].geo.replace(/^.+,(.+)$/, '$1'),
+                stay: data.points[i].stay
+            });
+        }
+
+        var friend_map;
+
+        ymaps.ready(function(){
+            friend_map = new ymaps.Map("first_map", {
+                center: [points[points.length-1].lat, points[points.length-1].lon],
+                zoom: 16
+            });
+
+            friend_map.controls.add('zoomControl');
+
+            // добавление линий на карту
+            var pps = [];
+            var cnt = 1;
+            for(key in points) {
+                var ballon = '';
+                // первую и последнюю точку отмечаем специальными значками если это не остановки
+                if (points[key].stay == 0 && (key == 0 || key == points.length-1)) {
+                    if (0 == key) {
+                        ballon = 'Начало маршрута';
+                    } else {
+                        ballon = 'Конец маршрута';
+                    }
+                    myGeoObject = new ymaps.GeoObject({
+                        geometry: {
+                            type: "Point",// тип геометрии - точка
+                            coordinates: [points[key].lat, points[key].lon] // координаты точки
+                        },
+                        properties: {
+                            balloonContent: ballon
+                        }
+                    }, {
+                        iconImageHref: '/i/ig.png',
+                    });
+
+                    friend_map.geoObjects.add(myGeoObject);
+                    cnt++;
+                }
+
+                pps.push([
+                    points[key].lat,
+                    points[key].lon
+                ]);
+
+
+                if (points[key].stay > 0) {
+                    if (points[key].stay < 60) {
+                        ballon = 'Остановка: ' + getFormatTime(points[key].stay);
+                    } else {
+                        ballon = 'Стоянка: ' + getFormatTime(points[key].stay);
+                    }
+
+                    myGeoObject = new ymaps.GeoObject({
+                        geometry: {
+                            type: "Point",// тип геометрии - точка
+                            coordinates: [points[key].lat, points[key].lon] // координаты точки
+                        },
+                        properties: {
+                            iconContent: cnt,
+                            balloonContent: ballon
+                        }
+                    });
+
+                    friend_map.geoObjects.add(myGeoObject);
+                    cnt++;
+                }
+
+            }
+
+            // https://tech.yandex.ru/maps/doc/jsapi/2.0/ref/reference/Polyline-docpage/
+            // Создаем ломаную линию.
+            var polyline = new ymaps.Polyline(pps, {
+                hintContent: "Маршрут"
+            }, {
+                draggable: false,
+                strokeColor: '#f00',
+                strokeWidth: 5,
+            });
+            // Добавляем линию на карту.
+            friend_map.geoObjects.add(polyline);
+            // Устанавливаем карте границы линии.
+            friend_map.setBounds(polyline.geometry.getBounds());
+        });
+    });
+}
+
+function getFormatTime (time) {
+    var second = time % 60;
+    var minute = Math.floor(time / 60) % 60;
+    var hour = Math.floor(time / 3600) % 60;
+
+    second = (second < 10) ? '0'+second : second;
+    minute = (minute < 10) ? '0'+minute : minute;
+    hour = (hour < 10) ? '0'+hour : hour;
+
+    return hour + ':' + minute + ':' + second;
 }
